29894b4d9e2370a3aaa6981b29fdc9196de0bc35
john Doe <negarshahbazi.official@gmail.com>
2024-01-16 15:24:48 +0100

    16jan

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-require_once('./class/Qcm.php');
-require_once('./class/Question.php');
-require_once('./class/Answer.php');
+function my_autoload($class){ 
+include 'class/' . $class . '.php';  
+}
+spl_autoload_register('my_autoload');
+// require_once('./class/Qcm.php');
+// require_once('./class/Question.php');
+// require_once('./class/Answer.php');
 require_once('./connexion.php');
 
 $qcm = new Qcm($db);
